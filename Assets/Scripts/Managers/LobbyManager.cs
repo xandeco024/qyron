e744a56de71431d97fdfa9777b9e9b2c6ee020d3
Xandeco Odyssey <alepbravo1702@gmail.com>
2024-05-21 22:13:49 -0300

    tentando entender player input manager

--- Assets/Scripts/Managers/LobbyManager.cs
+++ Assets/Scripts/Managers/LobbyManager.cs
@@ -1,38 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+class LobbyPlayer
+{
+    public PlayableCharacter character;
+    public InputDevice device;
+
+    public LobbyPlayer(PlayableCharacter character, InputDevice device)
+    {
+        this.character = character;
+        this.device = device;
+    }
+}
 
 public class LobbyManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
     private int playerCount = 0;
+    [SerializeField] PlayableCharacter[] playableCharacterPrefabs;
+    string[] selectedCharacters = new string[4];
+    private PlayerInputManager playerInputManager;
+
+    private LobbyPlayer[] lobbyPlayers = {null, null, null, null};
+
     void Start()
     {
-        
+        playerInputManager = GetComponent<PlayerInputManager>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void JoinPlayer()
     {
-
+        if (playerCount < 4)
+        {
+            lobbyPlayers[playerCount] = new LobbyPlayer(playableCharacterPrefabs[playerCount], null);
+        }
+        else
+        {
+            Debug.Log("Lobby is full.");
+        }
     }
 
-    public void OnPlayerJoined(PlayerInputManager.PlayerJoinedEvent playerJoinedEvent)
+    public void LeavePlayer(int playerIndex)
     {
-        playerCount++;
-        Debug.Log("Player " + playerCount + "Joined");
-    }
-
-    public void OnPlayerLeft(PlayerInputManager.PlayerLeftEvent playerLeftEvent)
-    {
-        playerCount--;
-        Debug.Log("Player " + playerCount + "Left");
+        if (lobbyPlayers[playerIndex] != null)
+        {
+            lobbyPlayers[playerIndex] = null;
+            playerCount--;
+        }
     }
 }
